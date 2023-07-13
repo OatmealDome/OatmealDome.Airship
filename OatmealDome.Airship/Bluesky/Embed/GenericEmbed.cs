f530cd883fe0002c76dc2b43cac995f45524c035
OatmealDome <julian@oatmealdome.me>
2023-07-13 15:19:28 -0400

    GenericEmbed: Create generic class for embed types

---
+++ OatmealDome.Airship/Bluesky/Embed/GenericEmbed.cs
@@ -0,0 +1,9 @@
+using System.Text.Json.Serialization;
+using OatmealDome.Airship.Bluesky.Embed.Json;
+
+namespace OatmealDome.Airship.Bluesky.Embed;
+
+public abstract class GenericEmbed
+{
+    //
+}
