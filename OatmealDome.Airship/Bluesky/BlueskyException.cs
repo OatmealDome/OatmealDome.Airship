df214448fd1c882fe7e242fc7a657d4028f23b1d
OatmealDome <julian@oatmealdome.me>
2024-10-14 14:41:07 -0400

    BlueskyException: Add exception class for Bluesky code

---
+++ OatmealDome.Airship/Bluesky/BlueskyException.cs
@@ -0,0 +1,14 @@
+namespace OatmealDome.Airship.Bluesky;
+
+public class BlueskyException : Exception
+{
+    public BlueskyException() : base()
+    {
+        //
+    }
+    
+    public BlueskyException(string message) : base(message)
+    {
+        //
+    }
+}
